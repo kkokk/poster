5c504b9f83e2f0d14740562cfd8c267b448b2dc6
lang <732853989@qq.com>
2025-07-07 11:14:55 +0800

    fix PHP Deprecated:  Implicit conversion from float  to int loses precision

--- src/PHPQrcode/phpqrcode.php
+++ src/PHPQrcode/phpqrcode.php
@@ -2953,11 +2953,11 @@
                 if($col >= $this->rsblocks[0]->dataLength) {
                     $row += $this->b1;
                 }
-                $ret = $this->rsblocks[$row]->data[$col];
+                $ret = $this->rsblocks[(int)$row]->data[(int)$col];
             } else if($this->count < $this->dataLength + $this->eccLength) {
                 $row = ($this->count - $this->dataLength) % $this->blocks;
                 $col = ($this->count - $this->dataLength) / $this->blocks;
-                $ret = $this->rsblocks[$row]->ecc[$col];
+                $ret = $this->rsblocks[$row]->ecc[(int)$col];
             } else {
                 return 0;
             }
