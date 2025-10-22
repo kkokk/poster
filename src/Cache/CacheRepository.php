d97ee827e68e46da5f7c100ee8c9c9161f54f01d
lang <732853989@qq.com>
2025-10-22 15:52:28 +0800

    修复：当验证器未自定义缓存器且没有内置缓存器时无法返回secret

--- src/Cache/CacheRepository.php
+++ src/Cache/CacheRepository.php
@@ -55,19 +55,25 @@ class CacheRepository
             return call_user_func_array([$this->adapter, $method], $params);
         }
 
-        // 兼容以前写法
-        if (class_exists(LaravelCache::class)) {
-            $connector = LaravelCache::class;
-        } elseif (class_exists(ThinkCache6::class)) {
-            $connector = ThinkCache6::class;
-            $method = str_replace('put', 'set', $method);
-        } elseif (class_exists(ThinkCache5::class)) {
-            $connector = ThinkCache5::class;
-            $method = str_replace('put', 'set', $method);
-        } else {
-            throw new PosterException('No cache driver');
+        try {
+            // 兼容以前写法
+            if (class_exists(LaravelCache::class)) {
+                $connector = LaravelCache::class;
+            } elseif (class_exists(ThinkCache6::class)) {
+                $connector = ThinkCache6::class;
+                $method = str_replace('put', 'set', $method);
+            } elseif (class_exists(ThinkCache5::class)) {
+                $connector = ThinkCache5::class;
+                $method = str_replace('put', 'set', $method);
+            } else {
+                throw new PosterException('No cache driver');
+            }
+
+            return call_user_func_array([$connector, $method], $params);
+        } catch (\Exception $e) {
+            // 无法从缓存检查，返回false
+            return false;
         }
-        return call_user_func_array([$connector, $method], $params);
     }
 
 }
